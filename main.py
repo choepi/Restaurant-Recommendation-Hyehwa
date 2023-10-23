5dce538fb7026e28ca1362c60a52dd1efc75eb1f
choepi <49416612+choepi@users.noreply.github.com>
2023-10-23 15:59:46 +0900

    -

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ text = Text(window, height=2, font=('courier', 12))
 text.pack() 
 button = Button(window, text='hit me', font=('Arial', 12), width=10, height=1)
 button.pack()
-window.mainloop() #keep display window and listen for request
+window.mainloop() #keep display window and listen for requests
 
 
 
