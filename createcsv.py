71740b084329cc85a82864f8bc22126c11d845a7
choepi <49416612+choepi@users.noreply.github.com>
2023-10-30 20:06:14 +0900

    asdfas

--- createcsv.py
+++ createcsv.py
@@ -1,11 +1,11 @@
 import csv
+import pandas as pd
 import random
 
 # Create sample restaurant names
 restaurant_names = [f"Restaurant_{i}" for i in range(100)]
 
 # Adjusted coordinates generation to be centered around Hyehwa, Seoul.
-# Coordinates for Hyehwa, Seoul: approximately 37.5822° N, 127.0019° E
 hyehwa_latitude = 37.5822
 hyehwa_longitude = 127.0019
 variance = 0.01  # about 1km
@@ -15,10 +15,22 @@ longitudes = [random.uniform(hyehwa_longitude - variance, hyehwa_longitude + var
 review_points = [random.randint(1, 5) for _ in range(100)]
 reviews = [f"Review for {name}" for name in restaurant_names]
 
+# Sample categories
+categories = [
+    "Korean", "Chinese", "Japanese", "Western", "Fusion",
+    "Fast Food", "Dessert", "Seafood", "Barbecue", "Vegetarian"
+]
+
+# Create a pandas DataFrame from the generated data
+data = pd.DataFrame({
+    "Name": restaurant_names,
+    "Latitude": latitudes,
+    "Longitude": longitudes,
+    "Review Point": review_points,
+    "Review": reviews,
+    "Category": [random.choice(categories) for _ in range(100)]
+})
+
 # Write the data to a CSV file
 csv_path = "sample_data.csv"
-with open(csv_path, 'w', newline='') as csvfile:
-    csvwriter = csv.writer(csvfile)
-    csvwriter.writerow(["Name", "Latitude", "Longitude", "Review Point", "Review"])  # Write header
-    for i in range(100):
-        csvwriter.writerow([restaurant_names[i], latitudes[i], longitudes[i], review_points[i], reviews[i]])
+data.to_csv(csv_path, index=False)
--- main.py
+++ main.py
@@ -3,20 +3,21 @@
 # pip install mysql-connector-python
 import requests
 import tkinter as tk
-from tkinter import ttk
+from tkinter import ttk, filedialog
 import webbrowser
 import folium
 import os
 import geocoder
 import mysql.connector
 from translate import LibreTranslate
-
+import pandas as pd
 
 class NaverApp(tk.Tk):
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
         self.title("NaverFood")
         self.iconbitmap("RDBMS_Project/naverfood.png")
+        
         # Create a container frame to hold the pages
         container = ttk.Frame(self)
         container.pack(side="top", fill="both", expand=True)
@@ -25,23 +26,10 @@ class NaverApp(tk.Tk):
         self.notebook = ttk.Notebook(container)
         self.notebook.pack(fill="both", expand=True)
         
-        # Mock database with sample search results categorized into three options
-        self.database = {
-            "Option 1": [
-                {"name": "Result 1A", "link": "https://example.com/result1A", "lat": 37.7749, "lon": -122.4194},
-                {"name": "Result 1B", "link": "https://example.com/result1B", "lat": 34.0522, "lon": -118.2437},
-            ],
-            "Option 2": [
-                {"name": "Result 2A", "link": "https://example.com/result2A", "lat": 40.7128, "lon": -74.0060},
-                {"name": "Result 2B", "link": "https://example.com/result2B", "lat": 41.8781, "lon": -87.6298},
-            ],
-            "Option 3": [
-                {"name": "Result 3A", "link": "https://example.com/result3A", "lat": 51.5074, "lon": -0.1278},
-                {"name": "Result 3B", "link": "https://example.com/result3B", "lat": 48.8566, "lon": 2.3522},
-            ]
-        }
-                
-        # Create a dictionary to hold pages
+        # Load the CSV data into a pandas DataFrame
+        self.database = pd.read_csv('sample_data.csv')
+        print(self.database)
+        # Create a dictionary to hold pages 
         self.pages = {}
         
         # Create and add pages to the dictionary
@@ -77,9 +65,30 @@ class MainPage(tk.Frame):
         self.filter_label.pack(pady=5)
         
         self.selected_category = tk.StringVar(value="None")  # Default is "None"
-        categories = ["None", "Category A", "Category B", "Category C"]
+        
+        # Fetch unique categories from the database
+        if 'Category' in self.controller.database.columns:
+            categories = ["None"] + self.controller.database['Category'].unique().tolist()
+        else:
+            categories = ["None", "Category A", "Category B", "Category C"]
+        
         for category in categories:
             radiobutton = tk.Radiobutton(self, text=category, variable=self.selected_category, value=category)
+            radiobutton.pack(anchor="w")
+        
+        # Radio buttons for English and Kids options
+        self.english_label = tk.Label(self, text="English:")
+        self.english_label.pack(pady=5)
+        self.english_option = tk.StringVar(value="No")
+        for option in ["Yes", "No"]:
+            radiobutton = tk.Radiobutton(self, text=option, variable=self.english_option, value=option)
+            radiobutton.pack(anchor="w")
+
+        self.kids_label = tk.Label(self, text="Kids:")
+        self.kids_label.pack(pady=5)
+        self.kids_option = tk.StringVar(value="No")
+        for option in ["Yes", "No"]:
+            radiobutton = tk.Radiobutton(self, text=option, variable=self.kids_option, value=option)
             radiobutton.pack(anchor="w")
         
         # Search button
@@ -88,24 +97,20 @@ class MainPage(tk.Frame):
         
         self.exit_button = tk.Button(self, text="Exit", command=self.controller.quit)
         self.exit_button.pack()
-    
-    def load_csv_to_db(self):
-        file_path = filedialog.askopenfilename(filetypes=[("CSV files", "*.csv")])
-        if not file_path:
-            return
 
+    def load_csv_to_db(self):
         # Load the CSV file into a pandas DataFrame
-        data = pd.read_csv(file_path)
+        data = self.database
 
         # Connect to MySQL
         conn = mysql.connector.connect(
             host="localhost",
             user="root",
-            password="1083"
+            password="your_password"
         )
         cursor = conn.cursor()
 
-        # Insert the data into the MySQL table
+        # Insert the data into the MySQL table (you'll need to adjust the placeholders and columns accordingly)
         for index, row in data.iterrows():
             cursor.execute("INSERT INTO your_table_name (column1, column2, ...) VALUES (%s, %s, ...)", 
                            (row['column1'], row['column2'], ...))
@@ -125,20 +130,6 @@ class MainPage(tk.Frame):
         search_page = self.controller.pages["SearchResult"]
         search_page.perform_search(query, filter_category)  
     
-    def fetch_data_from_db(self, query=None, category=None):
-        # Mock function simulating fetching data from the database based on search criteria.
-        # In a real scenario, this function would interact with your database.
-        mock_data = [
-            {"name": "Item 1", "category": "Category A"},
-            {"name": "Item 2", "category": "Category B"},
-            {"name": "Item 3", "category": "Category C"}
-        ]
-        if query:
-            mock_data = [item for item in mock_data if query.lower() in item["name"].lower()]
-        if category:
-            mock_data = [item for item in mock_data if category == item["category"]]
-        return mock_data
-
 class SearchResult(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
@@ -155,25 +146,33 @@ class SearchResult(tk.Frame):
         
         self.results_listbox.config(yscrollcommand=self.scrollbar.set)
         self.results_listbox.bind("<Double-Button-1>", self.select_result)
-        
-        self.exit_button = tk.Button(self, text="Exit", command=self.controller.quit)
-        self.exit_button.pack()
 
     def perform_search(self, query, category):
-        # Here, fetch the results based on the search criteria and display them in the Listbox.
-        # For demonstration, we're using mock data.
-        mock_results = ["Result 1", "Result 2", "Result 3", "Result 4"]  # Replace with actual results
-        for result in mock_results:
-            self.results_listbox.insert(tk.END, result)
+        # Filter the results based on the search criteria from the database (pandas DataFrame)
+        self.filtered_data = self.controller.database
+        
+        if query:
+            self.filtered_data = self.filtered_data[self.filtered_data['Name'].str.contains(query, case=False)]
+            
+        if category and category != "None":
+            self.filtered_data = self.filtered_data[self.filtered_data['Category'] == category]
+        
+        # Display the filtered results in the Listbox
+        for _, row in self.filtered_data.iterrows():
+            display_text = f"{row['Name']} (Lat: {row['Lat']}, Lon: {row['Lon']})"
+            self.results_listbox.insert(tk.END, display_text)
 
     def select_result(self, event):
-        # Get the selected result
-        selected_result = self.results_listbox.get(self.results_listbox.curselection())
+        # Get the selected result's index
+        index = self.results_listbox.curselection()[0]
+        
+        # Fetch the full record from the filtered_data DataFrame based on the index
+        selected_record = self.filtered_data.iloc[index]
         
         # Switch to the Result and display the details of the selected result
         self.controller.show_page("Result")
-        Result = self.controller.pages["Result"]
-        Result.display_details(selected_result)
+        result_page = self.controller.pages["Result"]
+        result_page.display_details(selected_record)
 
 class Result(tk.Frame):
     def __init__(self, parent, controller):
@@ -214,25 +213,33 @@ class Result(tk.Frame):
         self.exit_button = tk.Button(self, text="Exit", command=self.controller.quit)
         self.exit_button.pack()
 
-    def display_details(self, result):
+    def display_details(self, record):
         # Display the details of the selected result and show its location on the map.
-        # For demonstration, we're just printing the result. You can enhance this to fetch more details
-        # and display them in the GUI, as well as update the map location.
-        print(f"Displaying details for: {result}")
-        # Here, update the GUI widgets and map to reflect the details of the selected result.
+        # Update the result label with the restaurant's name
+        self.result_label.config(text=f"Result Page: {record['Name']}")
+        
+        # Update the map marker to the location of the selected restaurant
+        self.result_marker.location = [record['Lat'], record['Lon']]
+        self.result_marker.popup = folium.Popup(record['Name'])
+        
+        # Center the map around the selected restaurant's location
+        self.map.location = [record['Lat'], record['Lon']]
+        
+        # Refresh the map by saving it again
+        self.map.save(self.map_filepath)
 
     def copy_to_clipboard(self):
         # Here, for demonstration purposes, I'm copying the first link from "Option 1" in the database. 
         # You can modify this based on your requirements.
-        link = self.controller.database["Option 1"][0]["link"]
+        link = self.controller.database.loc[0, "Name"]  # Using pandas DataFrame for example
         self.clipboard_clear()
         self.clipboard_append(link)
         self.update()  # This is necessary to finalize the clipboard changes
 
-
 if __name__ == "__main__":
     app = NaverApp()
     app.mainloop()
+
 
     #cd D:\RDMS\RDBMS_Project\
     #pyinstaller --onefile --noconsole D:\RDMS\RDBMS_Project\main.py
